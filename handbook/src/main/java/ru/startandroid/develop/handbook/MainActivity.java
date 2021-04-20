f56048bfcfed6e0717c21340802a04ebd7a85ac9
leil25 <nuriaxmetova93@mail.ru>
2021-04-20 11:48:26 +0300

    Create Fisher Handbook
    We was create module "Handbook" for fishermen.
    It is consist of fish, corm, tools list.

--- handbook/src/main/java/ru/startandroid/develop/handbook/MainActivity.java
+++ handbook/src/main/java/ru/startandroid/develop/handbook/MainActivity.java
@@ -1,13 +1,19 @@
 package ru.startandroid.develop.handbook;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,35 +22,46 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private ListView list;
+    private String[] array;
+    private ArrayAdapter<String> adapter;
     private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        list = findViewById(R.id.listView);
+        array = getResources().getStringArray(R.array.fish_array);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<String>(Arrays.asList(array)));
+        list.setAdapter(adapter);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed(){
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if(drawer.isDrawerOpen(GravityCompat.START))
+        {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else
+            super.onBackPressed();
     }
 
     @Override
@@ -55,9 +72,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.fish:
+                array = getResources().getStringArray(R.array.fish_array);
+                adapter.clear();
+                adapter.addAll(array);
+                adapter.notifyDataSetChanged();
+                break;
+            case R.id.corm:
+                array = getResources().getStringArray(R.array.corm_array);
+                adapter.clear();
+                adapter.addAll(array);
+                adapter.notifyDataSetChanged();
+                break;
+            case R.id.tools:
+                array = getResources().getStringArray(R.array.sna_array);
+                adapter.clear();
+                adapter.addAll(array);
+                adapter.notifyDataSetChanged();
+                break;
+            case R.id.fisher_story:
+                array = getResources().getStringArray(R.array.history_array);
+                adapter.clear();
+                adapter.addAll(array);
+                adapter.notifyDataSetChanged();
+                break;
+            case R.id.advice:
+                array = getResources().getStringArray(R.array.advice_array);
+                adapter.clear();
+                adapter.addAll(array);
+                adapter.notifyDataSetChanged();
+                break;
+        }
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
